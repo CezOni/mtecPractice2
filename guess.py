50dedd4315ea4e2fa92b9c303a33db1160c3d4ad
Student <student@ent-v314-016.local>
2016-02-10 15:48:45 -0500

    class 4

---
+++ guess.py
@@ -0,0 +1,12 @@
+import sys
+
+guess = 12345
+answer = 12345
+
+
+
+if(guess == answer): 
+	print("You won!")
+
+if(guess != answer):
+	print("LOSER!")
---
+++ hello.py
@@ -0,0 +1,27 @@
+var = 1
+
+var1 = "hello"
+
+var2 = "1"
+
+var3 = 2
+
+print("hello world")
+
+print(type(var))
+
+print(type(var1))
+
+print(type(var4))
+
+#typecasting
+
+guess = "12345"
+print(type(guess))
+#Print it to show the before and after
+#grab the word guess and converts into
+troll = int(guess)
+print(troll)
+print(type(troll))
+print("You lose!!!!! The answer was:")
+print(troll + 1)
